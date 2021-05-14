65b91c56fc7f5f268bac88eb59ee0694b709dde2
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-14 15:59:46 -0600

    Added rules to the program

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -1,6 +1,7 @@
 #Validate a credit card using Luhn Algorithm
-#Rules: 11 number credit card
+#Rules: 16 number credit card
 #       Starting from the rightmost digit, double the value of every SECOND digit
-#       IF doubling of a number results in a number greater than 9, add the additional digts together.
+#       IF doubling of a number results in a number greater than 9, add the additional digits together.
 #       Sum up all of the digits (original un-doubled plus the doubled)
 #       If the total number is equal to 10 then the card is VALID
+
