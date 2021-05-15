f24c0c5f6a0cd76ac951072a29c5c470bf37bd97
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-15 14:23:23 -0600

    Added a function to prevent user from inputting anything but numbers

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -5,80 +5,44 @@
 #       Sum up all of the digits (original un-doubled plus the doubled)
 #       If the total number is equal to 10 then the card is VALID
 CCNumber = input('Please Input a Credit Card number\n')
-CCNumber2=str(CCNumber)
-IndexLength = int(len(CCNumber2)+1)
-InputLength = len(CCNumber2)
-if InputLength%2==0:
-    i=0
-    Total = 0
-    for OddDigit in CCNumber2:
-        if i==IndexLength-1:
-            break
-        elif int(CCNumber2[i])>4:
-            Niners = int(CCNumber2[i]) + int(CCNumber2[i])
-            Double = str(Niners)
-            Add = int(Double[0])+int(Double[1])
-            #print(Add)
-            Total += int(Add)
-            i += 2
+if CCNumber.isnumeric():
+    CCNumber2=str(CCNumber)
+    IndexLength = int(len(CCNumber2)+1)
+    InputLength = len(CCNumber2)
+    #Adding every OTHER number and doubling
+    if InputLength%2==0:
+        i=0
+        Total = 0
+        for OddDigit in CCNumber2:
+            if i==IndexLength-1:
+                break
+            elif int(CCNumber2[i])>4:
+                Niners = int(CCNumber2[i]) + int(CCNumber2[i])
+                Double = str(Niners)
+                Add = int(Double[0])+int(Double[1])
+                Total += int(Add)
+                i += 2
+            else:
+                Total+=int(CCNumber2[i])*2
+                i+=2
+#=================================================================
+        #Adding single digits
+        i=1
+        Total2=0
+        for EvenDigits in CCNumber2:
+            if i == IndexLength:
+                break
+            else:
+                Total2 += int(CCNumber2[i])
+                i+=2
+        #==================================================================
+        #Adding total of Even and Odd digits
+        Total3 = Total + Total2
+        if Total3%10==0:
+            print('Congratulations, your CC is valid!')
         else:
-            #print(CCNumber2[i])
-            Total+=int(CCNumber2[i])*2
-            i+=2
-    #print('Total is '+ str(Total))
-    #=================================================================
-    #Adding single digits
-    i=1
-    Total2=0
-    for EvenDigits in CCNumber2:
-        if i == IndexLength:
-            break
-        else:
-            #print(CCNumber2[i])
-            Total2 += int(CCNumber2[i])
-            i+=2
-    #print('Single Digit Total is: ' + str(Total2))
-    #==================================================================
-    #Adding total of Even and Odd digits
-    Total3 = Total + Total2
-    if Total3%10==0:
-        print('Congratulations, your CC is valid!')
+            print('Unfortunately your CC is NOT valid')
     else:
         print('Unfortunately your CC is NOT valid')
 else:
-    i = 0
-    Total = 0
-    for OddDigit in CCNumber2:
-        if i == IndexLength:
-            break
-        elif int(CCNumber2[i]) > 4:
-            Niners = int(CCNumber2[i]) + int(CCNumber2[i])
-            Double = str(Niners)
-            Add = int(Double[0]) + int(Double[1])
-            # print(Add)
-            Total += int(Add)
-            i += 2
-        else:
-            # print(CCNumber2[i])
-            Total += int(CCNumber2[i]) * 2
-            i += 2
-    # print('Total is '+ str(Total))
-    # =================================================================
-    # Adding single digits
-    i = 1
-    Total2 = 0
-    for EvenDigits in CCNumber2:
-        if i == IndexLength-1:
-            break
-        else:
-            # print(CCNumber2[i])
-            Total2 += int(CCNumber2[i])
-            i += 2
-    # print('Single Digit Total is: ' + str(Total2))
-    # ==================================================================
-    # Adding total of Even and Odd digits
-    Total3 = Total + Total2
-    if Total3 % 10 == 0:
-        print('Congratulations, your CC is valid!')
-    else:
-        print('Unfortunately your CC is NOT valid')
+    print("I'm sorry, that is not a valid input. Please input a 16 digit Credit Card Number")
