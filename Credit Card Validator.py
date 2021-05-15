a68cc066856afcb1613b39c09f4d4a4b43beae68
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-15 14:11:11 -0600

    Created another For and If else statements to avoid the index range error when user inputs string with odd length.

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -7,40 +7,78 @@
 CCNumber = input('Please Input a Credit Card number\n')
 CCNumber2=str(CCNumber)
 IndexLength = int(len(CCNumber2)+1)
-#=======================================================
-i=0
-Total = 0
-for OddDigit in CCNumber2:
-    if i==IndexLength-1:
-        break
-    elif int(CCNumber2[i])>4:
-        Niners = int(CCNumber2[i]) + int(CCNumber2[i])
-        Double = str(Niners)
-        Add = int(Double[0])+int(Double[1])
-        #print(Add)
-        Total += int(Add)
-        i += 2
+InputLength = len(CCNumber2)
+if InputLength%2==0:
+    i=0
+    Total = 0
+    for OddDigit in CCNumber2:
+        if i==IndexLength-1:
+            break
+        elif int(CCNumber2[i])>4:
+            Niners = int(CCNumber2[i]) + int(CCNumber2[i])
+            Double = str(Niners)
+            Add = int(Double[0])+int(Double[1])
+            #print(Add)
+            Total += int(Add)
+            i += 2
+        else:
+            #print(CCNumber2[i])
+            Total+=int(CCNumber2[i])*2
+            i+=2
+    #print('Total is '+ str(Total))
+    #=================================================================
+    #Adding single digits
+    i=1
+    Total2=0
+    for EvenDigits in CCNumber2:
+        if i == IndexLength:
+            break
+        else:
+            #print(CCNumber2[i])
+            Total2 += int(CCNumber2[i])
+            i+=2
+    #print('Single Digit Total is: ' + str(Total2))
+    #==================================================================
+    #Adding total of Even and Odd digits
+    Total3 = Total + Total2
+    if Total3%10==0:
+        print('Congratulations, your CC is valid!')
     else:
-        #print(CCNumber2[i])
-        Total+=int(CCNumber2[i])*2
-        i+=2
-#print('Total is '+ str(Total))
-#=================================================================
-#Adding single digits
-i=1
-Total2=0
-for EvenDigits in CCNumber2:
-    if i == IndexLength:
-        break
-    else:
-        #print(CCNumber2[i])
-        Total2 += int(CCNumber2[i])
-        i+=2
-#print('Single Digit Total is: ' + str(Total2))
-#==================================================================
-#Adding total of Even and Odd digits
-Total3 = Total + Total2
-if Total3%10==0:
-    print('Congratulations, your CC is valid!')
+        print('Unfortunately your CC is NOT valid')
 else:
-    print('Unfortunately your CC is NOT valid')
+    i = 0
+    Total = 0
+    for OddDigit in CCNumber2:
+        if i == IndexLength:
+            break
+        elif int(CCNumber2[i]) > 4:
+            Niners = int(CCNumber2[i]) + int(CCNumber2[i])
+            Double = str(Niners)
+            Add = int(Double[0]) + int(Double[1])
+            # print(Add)
+            Total += int(Add)
+            i += 2
+        else:
+            # print(CCNumber2[i])
+            Total += int(CCNumber2[i]) * 2
+            i += 2
+    # print('Total is '+ str(Total))
+    # =================================================================
+    # Adding single digits
+    i = 1
+    Total2 = 0
+    for EvenDigits in CCNumber2:
+        if i == IndexLength-1:
+            break
+        else:
+            # print(CCNumber2[i])
+            Total2 += int(CCNumber2[i])
+            i += 2
+    # print('Single Digit Total is: ' + str(Total2))
+    # ==================================================================
+    # Adding total of Even and Odd digits
+    Total3 = Total + Total2
+    if Total3 % 10 == 0:
+        print('Congratulations, your CC is valid!')
+    else:
+        print('Unfortunately your CC is NOT valid')
