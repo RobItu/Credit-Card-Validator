1dc1d61c26fb764d9983f957b83b87366225dee8
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-15 13:03:14 -0600

    Completed the single digit and other digit functions and added the totals together.

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -4,10 +4,10 @@
 #       IF doubling of a number results in a number greater than 9, add the additional digits together.
 #       Sum up all of the digits (original un-doubled plus the doubled)
 #       If the total number is equal to 10 then the card is VALID
-CCNumber = int(input('Please input a Credit Card Number\n'))
+CCNumber = 4809901048221026
 CCNumber2=str(CCNumber)
-IndexLength = int(len(CCNumber2)-2)
-#==========================================================================
+IndexLength = int(len(CCNumber2)+1)
+#=======================================================
 i=0
 Total = 0
 for OddDigit in CCNumber2:
@@ -17,11 +17,27 @@ for OddDigit in CCNumber2:
         Niners = int(CCNumber2[i]) + int(CCNumber2[i])
         Double = str(Niners)
         Add = int(Double[0])+int(Double[1])
-        #print(Add)
+        print(Add)
         Total += int(Add)
         i += 2
     else:
-        #print(CCNumber2[i])
-        Total+=int(CCNumber2[i])
+        print(CCNumber2[i])
+        Total+=int(CCNumber2[i])*2
         i+=2
 print('Total is '+ str(Total))
+#=================================================================
+#Adding single digits
+i=1
+Total2=0
+for EvenDigits in CCNumber2:
+    if i == IndexLength:
+        break
+    else:
+        print(CCNumber2[i])
+        Total2 += int(CCNumber2[i])
+        i+=2
+print('Single Digit Total is: ' + str(Total2))
+#==================================================================
+#Adding total of Even and Odd digits
+Total3 = Total + Total2
+print(Total3)
