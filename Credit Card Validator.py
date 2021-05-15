8090d072be097a2e2e40d17878a44b3a2ea9cf97
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-15 13:56:19 -0600

    Finished the validator, still has an index error problem if the lenght of the input string is odd.

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -4,7 +4,7 @@
 #       IF doubling of a number results in a number greater than 9, add the additional digits together.
 #       Sum up all of the digits (original un-doubled plus the doubled)
 #       If the total number is equal to 10 then the card is VALID
-CCNumber = 4809901048221026
+CCNumber = input('Please Input a Credit Card number\n')
 CCNumber2=str(CCNumber)
 IndexLength = int(len(CCNumber2)+1)
 #=======================================================
@@ -17,14 +17,14 @@ for OddDigit in CCNumber2:
         Niners = int(CCNumber2[i]) + int(CCNumber2[i])
         Double = str(Niners)
         Add = int(Double[0])+int(Double[1])
-        print(Add)
+        #print(Add)
         Total += int(Add)
         i += 2
     else:
-        print(CCNumber2[i])
+        #print(CCNumber2[i])
         Total+=int(CCNumber2[i])*2
         i+=2
-print('Total is '+ str(Total))
+#print('Total is '+ str(Total))
 #=================================================================
 #Adding single digits
 i=1
@@ -33,11 +33,14 @@ for EvenDigits in CCNumber2:
     if i == IndexLength:
         break
     else:
-        print(CCNumber2[i])
+        #print(CCNumber2[i])
         Total2 += int(CCNumber2[i])
         i+=2
-print('Single Digit Total is: ' + str(Total2))
+#print('Single Digit Total is: ' + str(Total2))
 #==================================================================
 #Adding total of Even and Odd digits
 Total3 = Total + Total2
-print(Total3)
+if Total3%10==0:
+    print('Congratulations, your CC is valid!')
+else:
+    print('Unfortunately your CC is NOT valid')
