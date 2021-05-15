1b060f8400d0ad9d145e2c035028d09a39582134
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-14 21:53:15 -0600

    Created For Loop to sequence every other digit

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -4,7 +4,7 @@
 #       IF doubling of a number results in a number greater than 9, add the additional digits together.
 #       Sum up all of the digits (original un-doubled plus the doubled)
 #       If the total number is equal to 10 then the card is VALID
-CCNumber = 4809901048221026
+CCNumber = 80584556
 CCNumber2=str(CCNumber)
 IndexLength = int(len(CCNumber2)-2)
 #=======================================================
@@ -13,8 +13,10 @@ for OddDigit in CCNumber2:
     if i==IndexLength:
         break
     elif int(CCNumber2[i])>4:
-        OddDigit = int(CCNumber2[i]) + int(CCNumber2[i])
-        print(OddDigit)
+        Niners = int(CCNumber2[i]) + int(CCNumber2[i])
+        Double = str(Niners)
+        Add = int(Double[0])+int(Double[1])
+        print(Add)
         i += 2
     else:
         print(CCNumber2[i])
