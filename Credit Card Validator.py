3eebf72183a852e60e26507ff4d2093a11440cf2
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-15 00:15:15 -0600

    Added adding function of digits that go over >9 in doubling.

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -6,12 +6,12 @@
 #       If the total number is equal to 10 then the card is VALID
 CCNumber = 4809901048221026
 CCNumber2=str(CCNumber)
-IndexLength = int(len(CCNumber2)-2)
+IndexLength = int(len(CCNumber2)+1)
 #=======================================================
 i=0
 Total = 0
 for OddDigit in CCNumber2:
-    if i==IndexLength:
+    if i==IndexLength-1:
         break
     elif int(CCNumber2[i])>4:
         Niners = int(CCNumber2[i]) + int(CCNumber2[i])
