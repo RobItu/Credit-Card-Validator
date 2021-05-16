d521ea51c0840664ead292c0d26576dfe4e03108
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-15 21:03:14 -0600

    Added date function and good through date.

--- Credit Card Validator.py
+++ Credit Card Validator.py
@@ -1,58 +1,94 @@
-import datetime
-from datetime import date
 #Validate a credit card using Luhn Algorithm
 #Rules: 16 number credit card
 #       Starting from the rightmost digit, double the value of every SECOND digit
 #       IF doubling of a number results in a number greater than 9, add the additional digits together.
 #       Sum up all of the digits (original un-doubled plus the doubled)
 #       If the total number is equal to 10 then the card is VALID
-CCNumber = input('Please Input a Credit Card number\n')
-CCDate = input('Good through date?')
-Today = date.today()
 
-#*************************************************************************************************
-#____Code for 16-digit number validation___
-if CCNumber.isnumeric():
-    CCNumber2=str(CCNumber)
-    IndexLength = int(len(CCNumber2)+1)
-    InputLength = len(CCNumber2)
-    #Adding every OTHER number and doubling
-    if InputLength%2==0:
-        i=0
-        Total = 0
-        for OddDigit in CCNumber2:
-            if i==IndexLength-1:
-                break
-            elif int(CCNumber2[i])>4:
-                Niners = int(CCNumber2[i]) + int(CCNumber2[i])
-                Double = str(Niners)
-                Add = int(Double[0])+int(Double[1])
-                Total += int(Add)
-                i += 2
-            else:
-                Total+=int(CCNumber2[i])*2
-                i+=2
-        #------------------------------------------------
+Counter = 1
+while Counter <4:
+    CCNumber = input('Please Input a 16 digit Credit Card number\n')
+    if CCNumber.isnumeric():
+        CCNumber2=str(CCNumber)
+        IndexLength = int(len(CCNumber2)+1)
+        InputLength = len(CCNumber2)
+        if InputLength%2==0:
+            i=0
+            Total = 0
+            for OddDigit in CCNumber2:
+                if i==IndexLength-1:
+                    break
+                elif int(CCNumber2[i])>4:
+                    Niners = int(CCNumber2[i]) + int(CCNumber2[i])
+                    Double = str(Niners)
+                    Add = int(Double[0])+int(Double[1])
+                    #print(Add)
+                    Total += int(Add)
+                    i += 2
+                else:
+                    #print(CCNumber2[i])
+                    Total+=int(CCNumber2[i])*2
+                    i+=2
+        #print('Total is '+ str(Total))
+#=================================================================
         #Adding single digits
-        i=1
-        Total2=0
-        for EvenDigits in CCNumber2:
-            if i == IndexLength:
+            i=1
+            Total2=0
+            for EvenDigits in CCNumber2:
+                if i == IndexLength:
+                    break
+                else:
+                    #print(CCNumber2[i])
+                    Total2 += int(CCNumber2[i])
+                    i+=2
+        #print('Single Digit Total is: ' + str(Total2))
+        #==================================================================
+        #Adding total of Even and Odd digits
+            Total3 = Total + Total2
+            if Total3%10==0:
+                print('Congratulations, your CC is valid!')
                 break
             else:
-                Total2 += int(CCNumber2[i])
-                i+=2
-        #--------------------------------------------------
-        #Adding total of Even and Odd digits
-        Total3 = Total + Total2
-        if Total3%10==0:
-            print('Congratulations, your CC is valid!')
-            print("Today's date is : " + str(Today))
+                print('Unfortunately your CC is NOT valid')
+                break
         else:
-            print('Unfortunately your CC is NOT valid')
+            i = 0
+            Total = 0
+            for OddDigit in CCNumber2:
+                if i == IndexLength:
+                    break
+                elif int(CCNumber2[i]) > 4:
+                    Niners = int(CCNumber2[i]) + int(CCNumber2[i])
+                    Double = str(Niners)
+                    Add = int(Double[0]) + int(Double[1])
+                # print(Add)
+                    Total += int(Add)
+                    i += 2
+                else:
+                # print(CCNumber2[i])
+                    Total += int(CCNumber2[i]) * 2
+                    i += 2
+        # print('Total is '+ str(Total))
+        # =================================================================
+        # Adding single digits
+            i = 1
+            Total2 = 0
+            for EvenDigits in CCNumber2:
+                if i == IndexLength-1:
+                    break
+                else:
+                # print(CCNumber2[i])
+                    Total2 += int(CCNumber2[i])
+                    i += 2
+        # print('Single Digit Total is: ' + str(Total2))
+        # ==================================================================
+        # Adding total of Even and Odd digits
+            Total3 = Total + Total2
+            if Total3 % 10 == 0:
+                print('Congratulations, your CC is valid!')
+            else:
+                print('Unfortunately your CC is NOT valid')
     else:
-        print('Unfortunately your CC is NOT valid')
-else:
-    print("I'm sorry, that is not a valid input. Please input a 16 digit Credit Card Number")
-#******************************************************************************************************************
-#________Date verification________
+        print("I'm sorry, that is not a valid input.")
+        #Attempt = input('You have ' + Left + ' attempts left')
+        Counter +=1
